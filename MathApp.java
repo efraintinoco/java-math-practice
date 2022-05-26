fe080f7a4f6137a2e4fbb8ef332e5a0c471adc06
efraintinoco <oakcliff_21@yahoo.com>
2022-05-25 21:43:34 -0500

    add solution

--- MathApp.java
+++ MathApp.java
@@ -1,5 +1,30 @@
 public class MathApp {
   public static void main(String[] args) {
-    System.out.println("Answer goes here");
+
+    int salary1 = 100000;
+    int salary2 = 50000;
+    System.out.println(Math.max(salary1, salary2));
+
+    int carPrice = 50000;
+    int truckPrice = 75000;
+    System.out.println(Math.min(carPrice, truckPrice));
+
+    double radius = 7.25;
+    System.out.println(Math.PI * (radius * radius));
+
+    double num1 = 5.0;
+    System.out.println(Math.sqrt(num1));
+
+    double x1 = 5;
+    double x2 = 10;
+    double y1 = 85;
+    double y2 = 50;
+    System.out.println(Math.hypot(x1 - x2, y1 - y2));
+
+    double num2 = -3.8;
+    System.out.println(Math.abs(num2));
+
+    double randomNumber = Math.random();
+    System.out.println(randomNumber);
   }
 }
